45cfb94707456097eb35c707072b31ef2a7b8eca
雪梟 <aoi.kaori@gmail.com>
2024-09-06 22:30:50 +0900

    2024/09/06

--- keyboards/fukuro/i_14/config.h
+++ keyboards/fukuro/i_14/config.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#define PMW33XX_CS_PIN B6 // SPI CS pin.
+#define SPI_SCK_PIN B1
+#define SPI_MISO_PIN B3
+#define SPI_MOSI_PIN B2
+
+/* Optional. */
+#define PMW33XX_CPI 800      // The CPI range is 100-12000, in increments of 100. Defaults to 1600 CPI.
+#define PMW33XX_CLOCK_SPEED 2000000 // Sets the clock speed that the sensor runs at. Defaults to 2000000
+#define MOUSE_EXTENDED_REPORT // Use -32767 to 32767, instead of just -127 to 127.
+// #define POINTING_DEVICE_TASK_THROTTLE_MS 10
+
 /* key matrix size */
-#define MATRIX_COLS 8
-#define MATRIX_ROWS 16
+#define MATRIX_COLS 4
+#define MATRIX_ROWS 4
 
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
@@ -10,7 +21,7 @@
 #define MATRIX_IO_DELAY 20
 
 #ifdef RGB_MATRIX_ENABLE
-#define DRIVER_LED_TOTAL 128
+#define DRIVER_LED_TOTAL 18
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 64 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
 #define RGB_MATRIX_DEFAULT_HUE 125 // Sets the default hue value, if none has been set
 #define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
--- keyboards/fukuro/i_14/keymaps/default/keymap.c
+++ keyboards/fukuro/i_14/keymaps/default/keymap.c
@@ -3,18 +3,30 @@
 
 #include QMK_KEYBOARD_H
 
+#define CPI_OPTIONS { 950 }
+#define CPI_DEFAULT 0
+#define ANGLE_OPTIONS { 215 }
+#define ANGLE_DEFAULT 0
+
+typedef union {
+    uint32_t raw;
+    struct {
+        uint8_t cpi_idx;
+        uint8_t angle_idx;
+    };
+} ballconfig_t;
+
+// BALL SETTINGS
+ballconfig_t ballconfig;
+uint8_t cpi;
+uint8_t angle;
+uint16_t cpi_array[] = CPI_OPTIONS;
+uint16_t angle_array[] = ANGLE_OPTIONS;
+#define CPI_OPTION_SIZE (sizeof(cpi_array) / sizeof(uint16_t))
+#define ANGLE_OPTION_SIZE (sizeof(angle_array) / sizeof(uint16_t))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
+
     [0] = LAYOUT(
         KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PAST,
