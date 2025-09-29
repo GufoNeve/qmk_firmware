6de0713b715b864cac229380d56f71214f7bd402
Fukuro <aoi.kaori@gmail.com>
2025-09-29 12:32:11 +0900

    Merge branch 'qmk:master' into master

---
+++ keyboards/handwired/5x6_split/rev1/config.h
@@ -0,0 +1,36 @@
+/* Copyright 2025 Zackarias Montell (@Wholteza)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+<<<<<<< HEAD:keyboards/omkbd/runner3680/5x7/config.h
+#define SELECT_SOFT_SERIAL_SPEED 1
+#define MASTER_RIGHT
+/*Sets the protocol speed when using serial communication*/
+//Speeds:
+//0: about 189kbps (Experimental only)
+//1: about 137kbps (default)
+//2: about 75kbps
+//3: about 39kbps
+//4: about 26kbps
+//5: about 20kbps
+=======
+#define SERIAL_USART_FULL_DUPLEX
+
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+#define MASTER_RIGHT
+>>>>>>> f31004ebec6903408f1b1687d89ba98900c82e4c:keyboards/handwired/5x6_split/rev1/config.h
